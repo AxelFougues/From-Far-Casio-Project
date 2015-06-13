e066b04e800d42942e1b012c65400b679a2d3f54
Az <az.axel@yahoo.com>
2015-06-13 15:45:32 -0300

    Animal gen. tested.
    It doesn't work...

--- AnimalPlant generator/LifeGene.c
+++ AnimalPlant generator/LifeGene.c
@@ -32,7 +32,8 @@ int tail[2];
 int feet[2];
 int neck[2];
 int horns[1];
-int eye[0];
+int eye[1];
+int i;
 int AddIn_main(int isAppli, unsigned short OptionNum){
     while(!IsKeyDown(KEY_CTRL_EXIT)){
         mainBody[0] = (rand() % 23)-3;
@@ -54,44 +55,47 @@ int AddIn_main(int isAppli, unsigned short OptionNum){
         horns[1] = (rand() % 3);
         eye[0] = (rand() % 2);
         display();
+        Sleep(500);
+        while(!IsKeyDown(KEY_CTRL_SHIFT)){
+            Sleep(4);
+        }
+        ML_clear_vram();
     }
-
     return 1;
 }
 void display(){
     ML_filled_ellipse_in_rect(x, y, x+mainBody[0], y+mainBody[1], mainBody[2]);
-    if(neck[0]){
-        ML_line(x, y, x+3, y+neck[2], 1);
-        if(neck[1]){
-            for (i = 1; i < (neck[1]/2); ++i){
-                ML_line(x, y+i, x+3, y+neck[2]+i, 1);
-            }
-            for (i = 1; i < (neck[1]/2); ++i){
-                ML_line(x, y-i, x+3, y+neck[2]-i, 1);
-            }
-            if(neck[0]>=2){
-                for (i = 0; i < (neck[1]/2-1); ++i){
-                    ML_line(x+3, y+neck[2], x+3+x+3, y+neck[2]+y+neck[2]+i, 1);
-                }
-                for (i = 0; i < (neck[1]/2-1); ++i){
-                    ML_line(x+3, y+neck[2], x+3+x+3, y+neck[2]+y+neck[2]-i, 1);
-                }
-            }
-            if(neck[0]>=3){
-                    ML_line(2*x+6, 2*y+neck[2], 3*x+9, 3*y+2*neck[2], 1);
-            }
-            if(neck[0]>=4){
-                    ML_line(3*x+9, 3*y+2*neck[2], 4*x+12, 4*y+2*neck[2], 1);
-            }
-        }
-        for (i = 0; i < head[2]; ++i){
-            ML_line(4*x+12, 4*y+2*neck[2]+i, 4*x+12+head[0], 4*y+2*neck[2]+i, 1);
-        }
-
-    }else{
-        ML_line(x+mainBody[0], y+mainBody[1]+i, x+mainBody[0]+head[0], y+mainBody[1]+i, 1);
-    }
-
+    //if(neck[0]){
+    //    ML_line(x, y, x+3, y+neck[2], 1);
+    //    if(neck[1]){
+    //        for (i = 1; i < (neck[1]/2); ++i){
+    //            ML_line(x, y+i, x+3, y+neck[2]+i, 1);
+    //        }
+    //        for (i = 1; i < (neck[1]/2); ++i){
+    //            ML_line(x, y-i, x+3, y+neck[2]-i, 1);
+    //        }
+    //        if(neck[0]>=2){
+    //            for (i = 0; i < (neck[1]/2-1); ++i){
+    //                ML_line(x+3, y+neck[2], x+3+x+3, y+neck[2]+y+neck[2]+i, 1);
+    //            }
+    //            for (i = 0; i < (neck[1]/2-1); ++i){
+    //                ML_line(x+3, y+neck[2], x+3+x+3, y+neck[2]+y+neck[2]-i, 1);
+    //            }
+    //        }
+    //        if(neck[0]>=3){
+    //                ML_line(2*x+6, 2*y+neck[2], 3*x+9, 3*y+2*neck[2], 1);
+    //        }
+    //        if(neck[0]>=4){
+    //                ML_line(3*x+9, 3*y+2*neck[2], 4*x+12, 4*y+2*neck[2], 1);
+    //        }
+    //    }
+    //    for (i = 0; i < head[2]; ++i){
+    //        ML_line(4*x+12, 4*y+2*neck[2]+i, 4*x+12+head[0], 4*y+2*neck[2]+i, 1);
+    //    }
+    //}else{
+    //    ML_line(x+mainBody[0], y+mainBody[1]+i, x+mainBody[0]+head[0], y+mainBody[1]+i, 1);
+    //}
+    ML_display_vram();
 }
 
 
